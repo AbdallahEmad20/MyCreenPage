06c3b295a58dec83a8405c4ad84cb9b3349a13f7
AbdallahEmad20 <116479217+AbdallahEmad20@users.noreply.github.com>
2024-03-24 22:00:05 +0200

    Add files via upload

---
+++ js/Lang.js
@@ -0,0 +1,170 @@
+let translations = {
+    ar:{
+     
+  word1:"فتحنا لك أبواب العالم   بمزايا استثنائية تتناسب  مع ذوقك وقيمك ",
+  word2:"أنظم إلى عالم كرين creen وكن الجزء الأكثر اشراقا ",
+  word3:"حمل التطبيق علي اندرويد و ايفون الأن" ,
+  word4:"افضل تطبيق متكامل",
+  word5:"الترفيه يعتمد على",
+  word6:"الصحة",
+  word7:"بيئة صحية أمنه",
+  word8:"التجميل",
+  word9:"شكل مميز وجذاب",
+  word10:"الرياضة",
+  word11:"بنية جسدية مناسبة",
+  word12:"معنا",
+  word13:"تفتح لك افاق مميزة ",
+  word14:"حمل التطبيق الآن",
+  word15:"حمل التطبيق الآن",
+  word16:"المميزات",
+  word17:"تجربة اجتماعية رياضية غير مسبوقة ",
+  word18:"تواصل بسهولة مع مدربك المميز",
+  word19:"تواصل بسهولة مع مدربك المميزتواصل بسهولة مع مدربك المميزتواصل بسهولة مع مدربك المميز",
+  word20:"امان عالي",
+  word21:"سياسة خصوصية محكمة",
+  word22:"محفظة خاصة  هدايا نقاط",
+  word23:" منتجات مميزة" ,
+  word24:"تواصل بسهولة مع مدربك المميزتواصل بسهولة مع مدربك المميزتواصل بسهولة مع مدربك المميز",
+  word25:"حوافز",
+  word26:"تحديات",
+  word27:"اطلب طعامك ووصفاتك المفضلة ",
+  word28:"في دقائق",
+  word29:"عبّر عن نفسك بحرية وأمان في بيئة صحية",
+  word30:"تقدر التنوع و الإختلاف",
+  word31:"أحدث تقنيات التواصل والفائدة ",
+  word32:"طرق دفع امنة وميسرة",
+  word33: "مع كرين ودّع تعقيدات الدفع عبر الإنترنت! نقدم لك مجموعة متنوعة من وسائل الدفع الآمنة والموثوقة لتضمن لك تجربة شراء سهلة وسلسة دون أي قلق. ثق في كرين لعمليات دفع آمنة وراحة بال!",
+  word34:"كل برامجك الرياضية بين يديك الان ",
+  word35:"اعرف رحلتك في الرياضة والتغذية للحصول إلى الهدف ",
+  word36:"عزز تعاملك الرياضي ",
+  word37:"ابحث عن مدربين رياضيين وأخصائيي تغذية محترفين بالقرب منك، واحجز جلساتك بسهولة لتحقيق أهدافك.",
+  word38:"اعرف رحلتك في الرياضة والتغذية  للحصول إلى الهدف ",
+  word39:"ضع أهدافك الرياضية وخططك الغذائية، وتابع تقدمك مع إحصائيات مخصصة لتحقيق النجاح.",
+  word40:"جرب الخيارات المتاحة الاكثر تفاعلا وذكاء ",
+  word41:"خليك مع كرين في امان على الدوام",
+  word42:"خليك مع كرين في امان على الدوام ",
+  word43:"رحلة واحدة فقط فيها محطات من أروع  المتع ",
+  word44:"أنضم إلى عالم كرين creen",
+  word45:"وكن الجزء الأكثر اشراقا ",
+  word46:"الشجاعة في التجربة وزينة العقل في حسن الاختيار ",
+  word47:"حمل التطبيق الآن",
+
+    },
+
+    en:{
+        word1:"We opened the doors of the world to you with exceptional benefits that suit your taste and values",
+        word2:"Join the world of Creen and be the brightest part",
+        word3:"Download the application on Android and iPhone now",
+        word4:"The best integrated applicationEntertainment depends on",
+        word5:"Entertainment depends on",
+        word6:"the health",
+        word7:"A safe, healthy environment",
+        word8:"Beautification",
+        word9:"Distinctive and attractive shape",
+       word10:"Sports",
+       word11:"Suitable physical structure",
+        word12:"With us",
+        word13:"It opens unique horizons for you",
+        word14:"Download the application now",
+        word15:"Download the application now",
+       word16:"Advantages",
+       word17:"An unprecedented social sports experience",
+        word18:" Easily communicate with your distinguished coach",
+        word19:"Communicate easily with your distinguished coach. Communicate easily with your distinguished coach. CommunicateEasily with your distinguished trainer",
+        word20:"High security",
+        word21:"Solid privacy policy ",
+        word22:"  Special wallet gift points",
+        word23:" Featured products",
+        word24:"Communicate easily with your distinguished coach. Communicate easily with your distinguished coach. CommunicateEasily with your distinguished trainer",
+
+        word25:" Incentives",
+        word26:"Challenges",
+        word27:"Order your favorite food and recipes",
+        word28:"In minutes",
+        word29:"Express yourself freely and safely in a healthy environment",
+        word30:"Appreciate diversity and difference",
+        word31:"The latest communication ",
+       word32: "Safe and easy payment methods",
+       word33:"With Crane, say goodbye to the complications of paying online! We offer you a variety of safe and reliable payment methods to ensure an easy and smooth purchasing experience without any worries. Trust Crane for secure payments and peace of mind!",  
+       word34:  " All your sports programs are in your hands no",
+       word35: "  Know your journey in sports and nutrition to reach your goa", 
+       word36:  "  Enhance your sporty handling",
+       word37:" Find professional sports trainers and nutritionists near you, and easily book your sessions to achieve your goals.",  
+       word38:"Know your journey in sports and nutrition to reach your goal",  
+       word39:" Set your sports goals and nutritional plans, and track your progress with statisticsDedicated to success.",  
+       word40:"Experience the most interactive and intelligent options available",  
+       word41: " Stay safe with Crane always", 
+       word42: "  Stay safe with Crane always", 
+       word43:" Only one trip contains stations of the most wonderful pleasures",  
+       word44: "  Join the world of creen", 
+       word45: " And be the brightest part", 
+       word46: "  Courage in experience and adornment of mind in making good choices", 
+       word47: " Download the application now", 
+   
+
+
+    }
+
+   
+  
+
+
+
+
+
+
+    
+    
+
+    
+    
+    
+   
+
+}
+
+// ======
+
+// let  ulInNav = document.querySelector(".navbar-nav")
+// console.log("ulInNav" , ulInNav)
+
+var titleHero =document.querySelector(".titleHero")
+const LanguageSelector =document.getElementById("Language-selector")
+LanguageSelector.addEventListener("change" , function (e){
+setLanguage (e.target.value)
+})
+
+ function setLanguage(language){
+    const elements =document.querySelectorAll("[data-lang]")
+
+    elements.forEach(element =>{
+
+       const  translateKey =element.getAttribute("data-lang")
+        element.innerHTML =translations[language][translateKey]
+        
+    })
+
+    // if(language === "ar" ){
+    //  document.body.dir ="rtl"
+     
+   
+
+    // }else{
+    // document.body.dir ="ltr"
+
+
+
+    // }
+
+    // if( language === "ar"){
+    
+    //     document.body.style.textAlign="right"
+  
+   
+
+    // }else if( language === "en"){
+    //     titleHero.style.fontSize="50px"
+    // document.body.style.textAlign="left"
+    // }
+
+}
